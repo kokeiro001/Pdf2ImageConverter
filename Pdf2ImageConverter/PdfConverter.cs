b5581329be82beb930d1d77935f82bfbc22b8f32
kokeiro <kokeiro.ssbm.001@gmail.com>
2019-08-04 01:04:43 +0900

    pdfからpng, jpegに変換する機能を実装した

---
+++ Pdf2ImageConverter/PdfConverter.cs
@@ -0,0 +1,74 @@
+﻿using Ghostscript.NET;
+using Ghostscript.NET.Processor;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// C:\Program Files (x86)\gs\gs9.27
+
+namespace Pdf2ImageConverter
+{
+    class PdfConverter
+    {
+        public enum ImageFormat
+        {
+            Png,
+            Jpeg,
+        }
+
+        public void ExtractImages(string inputFile, string outputFile, ImageFormat imageFormat)
+        {
+            var outputDirectory = Path.GetDirectoryName(outputFile);
+
+            if (Directory.Exists(outputDirectory))
+            {
+                Directory.Delete(outputDirectory, true);
+            }
+            Directory.CreateDirectory(outputDirectory);
+
+            using (GhostscriptProcessor ghostscript = new GhostscriptProcessor())
+            {
+                ghostscript.Processing += new GhostscriptProcessorProcessingEventHandler(ghostscript_Processing);
+
+                List<string> switches = new List<string>();
+                switches.Add("-empty");
+                switches.Add("-dSAFER");
+                switches.Add("-dBATCH");
+                switches.Add("-dNOPAUSE");
+                switches.Add("-dNOPROMPT");
+
+                switch (imageFormat)
+                {
+                    case ImageFormat.Png:
+                        switches.Add("-sDEVICE=png16m");
+                        switches.Add("-dTextAlphaBits=4");
+                        switches.Add("-dGraphicsAlphaBits=4");
+                        break;
+                    case ImageFormat.Jpeg:
+                        switches.Add("-sDEVICE=jpeg");
+                        switches.Add("-dJPEGQ=100");
+                        break;
+                    default:
+                        throw new Exception($"not support ImageFormat:{imageFormat}");
+                }
+                //switches.Add("-r96");
+                switches.Add("-r200");
+
+                switches.Add(@"-sOutputFile=" + outputFile);
+                switches.Add(@"-f");
+                switches.Add(inputFile);
+
+                ghostscript.Process(switches.ToArray());
+            }
+        }
+
+
+        void ghostscript_Processing(object sender, GhostscriptProcessorProcessingEventArgs e)
+        {
+            Console.WriteLine($"{e.CurrentPage}/{e.TotalPages}");
+        }
+    }
+}
--- Pdf2ImageConverter/Program.cs
+++ Pdf2ImageConverter/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Pdf2ImageConverter
 {
@@ -10,6 +7,93 @@ namespace Pdf2ImageConverter
     {
         static void Main(string[] args)
         {
+            var tmpPdfFilePath = @"C:\tmp\Pdf2ImageConverter\original.pdf";
+            var tmpDirectory = Path.GetDirectoryName(tmpPdfFilePath);
+
+            if (!Directory.Exists(tmpDirectory))
+            {
+                Directory.CreateDirectory(tmpDirectory);
+            }
+
+            var pdfConverter = new PdfConverter();
+
+            var sourceDirectries = new string[] 
+            {
+
+
+            };
+            var filePaths = sourceDirectries
+                .SelectMany(x => Directory.GetFiles(x, "*.pdf", SearchOption.TopDirectoryOnly));
+
+            var imageFormat = PdfConverter.ImageFormat.Png;
+            var extension = imageFormat.ToString().ToLower();
+
+            foreach (var sourceFilePath in filePaths)
+            {
+                // 日本語ファイルを扱えないため一時ファイルとしてコピーしてから作業する
+                File.Copy(sourceFilePath, tmpPdfFilePath, true);
+
+                var tmpOutputDirectory = Path.Combine(Path.GetDirectoryName(tmpPdfFilePath), extension);
+
+                var outputFile = Path.Combine(tmpOutputDirectory, $"%03d.{extension}");
+
+                pdfConverter.ExtractImages(tmpPdfFilePath, outputFile, imageFormat);
+
+                var sourceFileDirectory = Path.GetDirectoryName(sourceFilePath);
+                var sourceFileName = Path.GetFileNameWithoutExtension(sourceFilePath);
+
+                var outputDirectory = Path.Combine(sourceFileDirectory, sourceFileName);
+
+                if (Directory.Exists(outputDirectory))
+                {
+                    Directory.Delete(outputDirectory, true);
+                }
+
+                DirectoryCopy(tmpOutputDirectory, outputDirectory, true);
+            }
+
+            if (File.Exists(tmpPdfFilePath))
+            {
+                File.Delete(tmpPdfFilePath);
+            }
+        }
+
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
+            }
         }
     }
 }
