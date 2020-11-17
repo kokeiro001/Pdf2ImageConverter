dc6532989023113a042d09fa511c8969db5268e2
kokeiro001 <kokeiro.ssbm.001@gmail.com>
2020-11-18 00:51:22 +0900

    zipで固めるようにした

--- Pdf2ImageConverter/Program.cs
+++ Pdf2ImageConverter/Program.cs
@@ -1,11 +1,13 @@
+using System;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 
 namespace Pdf2ImageConverter
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             var tmpPdfFilePath = @"C:\tmp\Pdf2ImageConverter\original.pdf";
             var tmpDirectory = Path.GetDirectoryName(tmpPdfFilePath);
@@ -27,23 +29,37 @@ namespace Pdf2ImageConverter
 
             foreach (var sourceFilePath in filePaths)
             {
-                // 日本語ファイルを扱えないため一時ファイルとしてコピーしてから作業する
+                Console.WriteLine($"copy {sourceFilePath} -> {tmpPdfFilePath}");
+                // ライブラリが日本語ファイルを扱えないため一時ファイルとしてコピーしてから作業する
                 File.Copy(sourceFilePath, tmpPdfFilePath, true);
 
                 var tmpOutputDirectory = Path.Combine(Path.GetDirectoryName(tmpPdfFilePath), extension);
 
                 var tmpOutputFile = Path.Combine(tmpOutputDirectory, $"%03d.{extension}");
 
+                Console.WriteLine($"image extract from pdf");
                 pdfConverter.ExtractImages(tmpPdfFilePath, tmpOutputFile, imageFormat);
 
                 var sourceFileDirectory = Path.GetDirectoryName(sourceFilePath);
                 var sourceFileName = Path.GetFileNameWithoutExtension(sourceFilePath);
 
-                var outputDirectory = Path.Combine(sourceFileDirectory, sourceFileName + "_jpg");
+                var extractImagesDirectoryPath = Path.Combine(sourceFileDirectory, sourceFileName);
 
-                DirectoryUtility.DeleteDirectoryIfExists(outputDirectory);
+                DirectoryUtility.DeleteDirectoryIfExists(extractImagesDirectoryPath);
 
-                DirectoryUtility.Copy(tmpOutputDirectory, outputDirectory, true);
+                DirectoryUtility.Copy(tmpOutputDirectory, extractImagesDirectoryPath, true);
+
+                var zipOutputPath = Path.Combine(sourceFileDirectory, $"{sourceFileName}.zip");
+
+                Console.WriteLine($"CreateZip: {zipOutputPath}");
+
+                if (File.Exists(zipOutputPath))
+                {
+                    File.Delete(zipOutputPath);
+                }
+                ZipFile.CreateFromDirectory(extractImagesDirectoryPath, zipOutputPath);
+
+                DirectoryUtility.DeleteDirectoryIfExists(extractImagesDirectoryPath);
             }
 
             if (File.Exists(tmpPdfFilePath))
